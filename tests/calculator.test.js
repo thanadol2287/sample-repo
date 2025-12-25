baf305c6aace89de21feac0d8961bfd3f13e87bf
Thanadol Buntitanusorn <thanadol.b@ku.thsorn>
2025-12-25 15:03:50 +0700

    edit add

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -5,7 +5,7 @@ describe('Calculator', () => {
         expect(add(2, 3)).toBe(5);
     });
 
-    test('adds -2 + -3 to equal 5', () => {
+    test('adds -2 + -3 to equal -5', () => {
         expect(add(-2, -3)).toBe(-5);
     });
 
